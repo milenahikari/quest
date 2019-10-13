12063db66150bb2309d3aedb8d89aa5ab15b46ba
Milena <milenahikari11@gmail.com>
2019-10-13 20:18:13 -0300

    Salvando alteraçoes

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: 'https://backend-quest.herokuapp.com/api',
+  // baseURL: 'http://localhost:8000/api',
 });
 
 export default api;
