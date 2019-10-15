520e740d1f00725015e6bfd137cf9fb86d23466b
Milena <milenahikari11@gmail.com>
2019-10-15 20:57:27 -0300

    Adicionando configuracoes do whatsApp

--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,6 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 
 import user from './modules/user/root';
-import monitor from './modules/monitor/root';
-import monitors from './modules/monitors/root';
 import category from './modules/category/root';
 import auth from './modules/auth/root';
 import createPersistedState from 'vuex-persistedstate';
@@ -11,7 +9,7 @@ import createPersistedState from 'vuex-persistedstate';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  modules: { user, monitor, monitors, category, auth },
+  modules: { user, category, auth },
   plugins: [createPersistedState({
     paths: ['user', 'category', 'auth']
   })],
