cdbbd1a92062d0ee3684ae6c55f9ca90396315d2
Milena <milenahikari11@gmail.com>
2019-10-16 23:24:33 -0300

    Finalizando a avaliacao de estrelas na home

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: 'https://backend-quest.herokuapp.com/api',
-  baseURL: 'http://localhost:8000/api',
+  baseURL: 'https://backend-quest.herokuapp.com/api',
+  // baseURL: 'http://localhost:8000/api',
 });
 
 export default api;
