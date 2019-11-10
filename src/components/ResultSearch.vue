3b61d53bb0876c7251d2f11b6b743f35a92a49ac
Milena <milenahikari11@gmail.com>
2019-11-10 11:45:46 -0300

    Mensagem de pesquisa nao encontrada

--- src/components/ResultSearch.vue
+++ src/components/ResultSearch.vue
@@ -2,25 +2,31 @@
   <v-container grid-list-xl>
     <v-layout>
       <v-flex>
-        <h2 v-if="monitors.length > 0">Resultados encontrados...</h2>
+        <div v-if="monitors.length > 0">
+          <h2>Resultados encontrados...</h2>
 
-        <div 
-          v-for="monitor in monitors"
-          :key="monitor.id"
-        >
-          <CardMonitor 
-            :idMonitor="`${monitor.id}`"
-            :name="`${monitor.name_monitor}`" 
-            :course="`${monitor.course}`"
-            :email="`${monitor.email}`"
-            :city="`${monitor.city}`"
-            :state="`${monitor.state}`"
-            :rating="`${monitor.rating}`"
-            :phone="`${monitor.phone}`"
-            :share="`${monitor.share}`"
-            :photo="`${monitor.photo}`"
-          ></CardMonitor>
+          <div 
+            v-for="monitor in monitors"
+            :key="monitor.id"
+          >
+            <CardMonitor 
+              :idMonitor="`${monitor.id}`"
+              :name="`${monitor.name_monitor}`" 
+              :course="`${monitor.course}`"
+              :email="`${monitor.email}`"
+              :city="`${monitor.city}`"
+              :state="`${monitor.state}`"
+              :rating="`${monitor.rating}`"
+              :phone="`${monitor.phone}`"
+              :share="`${monitor.share}`"
+              :photo="`${monitor.photo}`"
+            ></CardMonitor>
 
+          </div>
+        </div>
+
+        <div v-else>
+          <h2>Não encontramos nenhum resultado para sua busca...</h2>
         </div>
 
       </v-flex>
